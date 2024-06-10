ce33c88b5f9d3051479162c9952fc1c5874c7279
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-10 21:45:39 +0200

    API created

---
+++ app.js
@@ -0,0 +1,84 @@
+{
+  "products": [
+    {
+      "id": 1,
+      "name": "T-Shirt",
+      "description": "Zeige deinen Stil mit unseren Grafik-T-Shirts.",
+      "category": "Clothing",
+      "price": 29.99,
+      "image_url": "./img/img-1-min.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Kapuzenshirt",
+      "description": "Erlebe Streetwear mit unseren bequemen und stylischen Kapuzenshirts.",
+      "category": "Clothing",
+      "price": 49.99,
+      "image_url": "./img/img-2-min.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Sneaker",
+      "description": "Fühle den Rhythmus der Stadt mit unseren Sneakern.",
+      "category": "Footwear",
+      "price": 89.99,
+      "image_url": "./img/img-3-min.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Mütze",
+      "description": "Vervollständige dein Outfit: Streetstyle-Mützen.",
+      "category": "Accessories",
+      "price": 19.99,
+      "image_url": "./img/img-4-min.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Jacke",
+      "description": "Mit stylischen und funktionalen Jacken jede Saison rocken.",
+      "category": "Clothing",
+      "price": 99.99,
+      "image_url": "./img/img-5-min.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Jogginghose",
+      "description": "Komfort und Stil vereint in unseren Jogginghosen.",
+      "category": "Clothing",
+      "price": 39.99,
+      "image_url": "./img/img-6-min.jpg"
+    },
+    {
+      "id": 7,
+      "name": "Tasche",
+      "description": "Stylische Taschen für deine täglichen Bedürfnisse.",
+      "category": "Accessories",
+      "price": 59.99,
+      "image_url": "./img/img-7-min.jpg"
+    },
+    {
+      "id": 8,
+      "name": "Sonnenbrille",
+      "description": "Setze ein Statement mit unseren ikonischen Sonnenbrillen.",
+      "category": "Accessories",
+      "price": 24.99,
+      "image_url": "./img/img-8-min.jpg"
+    },
+    {
+      "id": 9,
+      "name": "Sweatshirt",
+      "description": "Weiche und bequeme Sweatshirts für den Streetlook.",
+      "category": "Clothing",
+      "price": 59.99,
+      "image_url": "./img/img-9-min.jpg"
+    },
+    {
+      "id": 10,
+      "name": "Shorts",
+      "description": "Genieße den Sommer mit unseren bequemen und stylischen Shorts.",
+      "category": "Clothing",
+      "price": 34.99,
+      "image_url": "./img/img-10-min.jpg"
+    }
+  ]
+}
